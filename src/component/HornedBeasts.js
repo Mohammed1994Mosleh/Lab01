6a9b3bafc39028bbc9801adf413ee070b654ae49
Mohammed1994Mosleh <mohammed1994mosleh@gmail.com>
2021-08-09 17:40:03 +0300

    solve all features

--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 
 
 class HornedBeasts extends React.Component {
@@ -23,13 +25,25 @@ class HornedBeasts extends React.Component {
 
     render() {
         return (
-            <div>
-                <h2>{this.props.name}</h2>
-                <img src={this.props.imageUrl} onClick={this.incrNofp} alt='horn animal' title={this.props.name} ></img>
-                <p>{this.props.description}</p>
-                <p>❤ {this.state.numberOfpets}</p>
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={this.props.imageUrl} />
+            <Card.Body>
+            <Card.Title>{this.props.name}</Card.Title>
+            <Card.Text>
+            {this.props.description}
+            {this.state.numberOfpets}
+             </Card.Text>
+          <Button onClick={this.incrNofp} variant="primary">Votee</Button>
+            </Card.Body>
+          </Card>
 
-            </div>
+            // <div>
+            //     <h2>{this.props.name}</h2>
+            //     <img src={this.props.imageUrl} onClick={this.incrNofp} alt='horn animal' title={this.props.name} ></img>
+            //     <p>{this.props.description}</p>
+            //     <p>❤ {this.state.numberOfpets}</p>
+
+            // </div>
         )
 
 
