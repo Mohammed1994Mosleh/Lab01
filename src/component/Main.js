4bc38d8bbe3c493ea25ae58719e70c02717e7339
Mohammed1994Mosleh <mohammed1994mosleh@gmail.com>
2021-08-10 14:43:29 +0300

    lab03 without strech goals

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
 import hornData from '../data.json';
+import Modle from './Modle';
 
 
 class Main extends React.Component{
@@ -9,11 +10,11 @@ class Main extends React.Component{
    return(
 
      <div>
-       
+         <Modle/>
          {hornData.map((item,index)=>{
 
          return(
-         <HornedBeasts  key={index} name={item.title} imageUrl={item.image_url} description={item.description} />
+         <HornedBeasts   key={index} name={item.title} imageUrl={item.image_url} description={item.description} />
 
 
          
