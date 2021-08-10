5cd01f93dce56651e080a4fc0f5611c3adfc2409
Mohammed1994Mosleh <mohammed1994mosleh@gmail.com>
2021-08-10 08:49:24 +0300

    solve strech goal

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import './Header.css'
 
-class Header extends React.Component{
 
-    render(){
-     return(
-        <h1>Welcome</h1>
-     )
+let arr=['hi','welcome','hello','mohammed','mosleh','ahmed'];
+class Header extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state={
+            click:false,
+            newWord:'hornes gallery',
+            css:'./Header.css'
+        }
+
+    }
+changestate=()=>{
+
+    this.setState({
+        click:true,
+        newWord:arr[Math.floor(Math.random() * arr.length)],
+
+
+    })
+
+   
+
+}
+
+    render() {
+        return (
+            <h1 onClick={this.changestate}>{this.state.newWord}</h1>
+        )
 
 
     }
